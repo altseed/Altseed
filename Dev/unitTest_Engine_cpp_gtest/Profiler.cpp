8ddccbfb4cc4374d8e7f7a973817b22b3d0e0af3
swd <durl.swda@gmail.com>
2013-11-08 11:52:49 -0800

    update gtest imply profiler on linux(not debug yet)

--- Dev/unitTest_Engine_cpp_gtest/Profiler.cpp
+++ Dev/unitTest_Engine_cpp_gtest/Profiler.cpp
@@ -6,26 +6,32 @@
 
 void Profiler_Profiling()
 {
-	auto engine = ace::GetEngine();
-
-	engine->Initialize(ace::ToAString("Profiling").c_str(), 640, 480, false);
-
-	auto profiler = ace::GetProfiler();
-
-	printf("printfで負荷をかけます\n");
-	while (engine->DoEvents())
 	{
-		profiler->Start(12);
-		engine->Update();
-		profiler->End(12);
+		auto engine = ace::GetEngine();
+		ASSERT_TRUE(engine != nullptr);
 
-		profiler->Start(17);
-		printf( "*" );
-		profiler->End(17);
+		engine->Initialize(ace::ToAString("Profiling").c_str(), 640, 480, false);
+
+		auto profiler = ace::GetProfiler();
+
+		printf("printfで負荷をかけます\n");
+		while (engine->DoEvents())
+		{
+			profiler->Start(12);
+			engine->Update();
+			profiler->End(12);
+
+			profiler->Start(17);
+			printf( "*" );
+			profiler->End(17);
+		}
+		printf("\n");
+
+		engine->Terminate();
 	}
-	printf("\n");
 
-	engine->Terminate();
+	auto ref = ace::GetGlobalReferenceCount();
+	ASSERT_TRUE(ref == 0);
 }
 
 TEST(Profiler, Profiling)
