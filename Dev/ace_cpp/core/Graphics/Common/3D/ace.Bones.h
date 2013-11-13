2abb89ed5e74389993741e5e4a8a0a3e6648d89b
durswd <swda.durl@gmail.com>
2013-11-13 20:35:22 +0900

    リネーム

--- Dev/ace_cpp/core/Graphics/Common/3D/ace.Bones.h
+++ Dev/ace_cpp/core/Graphics/Common/3D/ace.Bones.h
@@ -4,6 +4,7 @@
 #include <Math/ace.Vector2DI.h>
 #include <Math/ace.Vector2DF.h>
 #include <Math/ace.Vector3DF.h>
+#include <Math/ace.Matrix44.h>
 #include <Graphics/ace.Color.h>
 
 #include "../../../ace.Core.Base.h"
@@ -11,9 +12,18 @@
 
 namespace ace
 {
-	class Armature
+	class Bones
 	{
 	private:
+		struct Bone
+		{
+			astring		Name;
+			int32_t		ParentBoneIndex;
+			Matrix44	LocalMat;
+			Matrix44	GlobalMatInv;
+		};
+
+		std::vector<Bone>	m_bones;
 
 	public:
 
