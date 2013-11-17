0bf39900960a5d18dd8ec3a54953e7725ddb4334
ArGxento <argxento@gmail.com>
2013-11-18 03:37:06 +0900

    平滑化フィルタをSetFilterを使った13*13に変更

--- Dev/ace_cpp/engine/ObjectSystem/PostEffect/ace.PostEffectSmoothing.cpp
+++ Dev/ace_cpp/engine/ObjectSystem/PostEffect/ace.PostEffectSmoothing.cpp
@@ -16,14 +16,19 @@ float4 main( const PS_Input Input ) : SV_Target
 {
 	uint width, height;
 	g_texture.GetDimensions(width, height);
-	float2 unit = float2(1.0 / width, 0);
+	float2 accum = float2(1.5 / width, 0);
+	float2 adder = float2(2.0 / width, 0);
 	float4 output = g_texture.Sample(g_sampler, Input.UV);
 	if(output.a == 0.0f) discard;
-	output += g_texture.Sample(g_sampler, Input.UV + unit);
-	output += g_texture.Sample(g_sampler, Input.UV - unit);
-	output += g_texture.Sample(g_sampler, Input.UV + unit * 2);
-	output += g_texture.Sample(g_sampler, Input.UV - unit * 2);
-	return output / 5.0;
+	output += g_texture.Sample(g_sampler, Input.UV + accum);
+	output += g_texture.Sample(g_sampler, Input.UV - accum);
+	accum += adder;
+	output += g_texture.Sample(g_sampler, Input.UV + accum);
+	output += g_texture.Sample(g_sampler, Input.UV - accum);
+	accum += adder;
+	output += g_texture.Sample(g_sampler, Input.UV + accum);
+	output += g_texture.Sample(g_sampler, Input.UV - accum);
+	return output / 7.0;
 }
 
 )";
@@ -35,12 +40,17 @@ uniform sampler2D g_texture;
 void main()
 {
 	vec4 output = texture2D(g_texture, inUV.xy);
-	vec2 unit = vec2(1.0 / textureSize(g_texture, 0).x, 0);
-	output += texture2D(g_texture, inUV.xy + unit);
-	output += texture2D(g_texture, inUV.xy - unit);
-	output += texture2D(g_texture, inUV.xy + unit * 2);
-	output += texture2D(g_texture, inUV.xy - unit * 2);
-	gl_FragColor = output / 5.0; 
+	vec2 accum = vec2(1.5 / textureSize(g_texture, 0).x, 0);
+	vec2 adder = vec2(2.0 / textureSize(g_texture, 0).x, 0);
+	output += texture2D(g_texture, inUV.xy + accum);
+	output += texture2D(g_texture, inUV.xy - accum);
+	accum += adder;
+	output += texture2D(g_texture, inUV.xy + accum);
+	output += texture2D(g_texture, inUV.xy - accum);
+	accum += adder;
+	output += texture2D(g_texture, inUV.xy + accum);
+	output += texture2D(g_texture, inUV.xy - accum);
+	gl_FragColor = output / 7.0; 
 }
 
 )";
@@ -55,14 +65,19 @@ float4 main( const PS_Input Input ) : SV_Target
 {
 	uint width, height;
 	g_texture.GetDimensions(width, height);
-	float2 unit = float2(0, 1.0 / height);
+	float2 accum = float2(0, 1.5 / height);
+	float2 adder = float2(0, 2.0 / height);
 	float4 output = g_texture.Sample(g_sampler, Input.UV);
 	if(output.a == 0.0f) discard;
-	output += g_texture.Sample(g_sampler, Input.UV + unit);
-	output += g_texture.Sample(g_sampler, Input.UV - unit * 1);
-	output += g_texture.Sample(g_sampler, Input.UV + unit * 2);
-	output += g_texture.Sample(g_sampler, Input.UV - unit * 2);
-	return output / 5.0;
+	output += g_texture.Sample(g_sampler, Input.UV + accum);
+	output += g_texture.Sample(g_sampler, Input.UV - accum);
+	accum += adder;
+	output += g_texture.Sample(g_sampler, Input.UV + accum);
+	output += g_texture.Sample(g_sampler, Input.UV - accum);
+	accum += adder;
+	output += g_texture.Sample(g_sampler, Input.UV + accum);
+	output += g_texture.Sample(g_sampler, Input.UV - accum);
+	return output / 7.0;
 }
 
 )";
@@ -74,12 +89,17 @@ uniform sampler2D g_texture;
 void main()
 {
 	vec4 output = texture2D(g_texture, inUV.xy);
-	vec2 unit = vec2(0, 1.0 / textureSize(g_texture, 0).y);
-	output += texture2D(g_texture, inUV.xy + unit);
-	output += texture2D(g_texture, inUV.xy - unit);
-	output += texture2D(g_texture, inUV.xy + unit * 2);
-	output += texture2D(g_texture, inUV.xy - unit * 2);
-	gl_FragColor = output / 5.0; 
+	vec2 accum = vec2(0, 1.5 / textureSize(g_texture, 0).y);
+	vec2 adder = vec2(0, 2.0 / textureSize(g_texture, 0).y);
+	output += texture2D(g_texture, inUV.xy + accum);
+	output += texture2D(g_texture, inUV.xy - accum);
+	accum += adder;
+	output += texture2D(g_texture, inUV.xy + accum);
+	output += texture2D(g_texture, inUV.xy - accum);
+	accum += adder;
+	output += texture2D(g_texture, inUV.xy + accum);
+	output += texture2D(g_texture, inUV.xy - accum);
+	gl_FragColor = output / 7.0; 
 }
 
 )";
@@ -115,7 +135,7 @@ void main()
 
 		m_material2dX = g->CreateMaterial2D(m_shaderX);
 
-
+		
 
 		if (g->GetGraphicsType() == ace::GRAPHICS_TYPE_DX11)
 		{
@@ -144,17 +164,23 @@ void main()
 
 	void PostEffectSmoothing::OnDraw(std::shared_ptr<RenderTexture2D> dst, std::shared_ptr<RenderTexture2D> src)
 	{
-		
+		const eTextureFilterType origSrcFiter = src->GetFilter();
+
+		src->SetFilter(eTextureFilterType::TEXTURE_FILTER_LINEAR);
 		m_material2dX->SetTexture2D(ace::ToAString("g_texture").c_str(), src);
 		auto size = src->GetSize();
 		auto type = src->GetType();
 		if (size.X <= 0 || size.Y <= 0 || m_graphics == nullptr){ return; } // return if the source and/or the graphics are invalid
 		auto tmp = m_graphics->CreateRenderTexture(size.X, size.Y, ace::eTextureFormat::TEXTURE_FORMAT_RGBA8888);
 		if (tmp->GetSize().X != size.X || tmp->GetSize().Y != size.Y || tmp->GetType() != type){ return; }
+		tmp->SetFilter(eTextureFilterType::TEXTURE_FILTER_LINEAR);
 		DrawOnTexture2DWithMaterial(tmp, m_material2dX);
 
 		m_material2dY->SetTexture2D(ace::ToAString("g_texture").c_str(), tmp);
+
+		
 		DrawOnTexture2DWithMaterial(dst, m_material2dY);
+		tmp->SetFilter(origSrcFiter);
 	}
 
 }
