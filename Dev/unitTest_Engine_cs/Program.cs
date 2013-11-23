8997056ae3a01840248146f7699ab1a019b9c406
swd <swda.durl@gmail.com>
2013-11-23 20:44:53 +0900

    CS単体テスト追加中

---
+++ Dev/unitTest_Engine_cs/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace unitTest_Engine_cs
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var e = new Window.Empty();
+			e.Test();
+		}
+	}
+}
---
+++ Dev/unitTest_Engine_cs/Window/Empty.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+
+namespace unitTest_Engine_cs.Window
+{
+	[TestFixture]
+	public class Empty
+	{
+		[Test]
+		public void Test()
+		{
+			var option = new ace.EngineOption
+			{
+				GraphicsType = ace.GraphicsType.DirectX11,
+				IsFullScreen = false
+			};
+			var initialized = ace.Engine.Initialize("test", 640, 480, option);
+
+			int time = 0;
+
+			while (ace.Engine.DoEvents())
+			{
+				ace.Engine.Update();
+
+				if (time == 10) break;
+				time++;
+			}
+
+			ace.Engine.Terminate();
+		}
+	}
+}
