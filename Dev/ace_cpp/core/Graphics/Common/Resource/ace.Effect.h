b86511acf21e2d9f39e0a7ab381f985af436f7bb
durswd <swda.durl@gmail.com>
2014-01-28 23:55:34 +0900

    Effekseerを追加

--- Dev/ace_cpp/core/Graphics/Common/Resource/ace.Effect.h
+++ Dev/ace_cpp/core/Graphics/Common/Resource/ace.Effect.h
@@ -1,12 +1,20 @@
-
+﻿
 #pragma once
 
 #include <ace.common.Base.h>
+#include "../../../ace.ReferenceObject.h"
 
 namespace ace
 {
 	class Effect
+		: public IReference
 	{
+	private:
+
+	protected:
+		Effect(){}
+		virtual ~Effect(){}
+	public:
 
 	};
 }
---
+++ Dev/ace_cpp/core/Graphics/Common/Resource/ace.Effect_Imp.h
@@ -0,0 +1,31 @@
+﻿
+#pragma once
+
+#include "ace.Effect.h"
+
+namespace ace
+{
+	class Effect_Imp
+		: public Effect
+		, public ReferenceObject
+	{
+	private:
+		Effekseer::Effect*	m_effect;
+
+		Effect_Imp(Effekseer::Effect* effect);
+		virtual ~Effect_Imp();
+	public:
+
+		static Effect_Imp* CreateEffect(Graphics* graphics);
+		
+		Effekseer::Effect* GetEffect() { return m_effect; }
+
+		// IReferenceを継承したデバイスオブジェクト向け定義
+#if !SWIG
+	public:
+		virtual int GetRef() { return ReferenceObject::GetRef(); }
+		virtual int AddRef() { return ReferenceObject::AddRef(); }
+		virtual int Release() { return ReferenceObject::Release(); }
+#endif
+	};
+}
