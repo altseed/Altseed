dc80d3dd8d77d3eef04659d3d14dc8e7734ddf6e
durswd <swda.durl@gmail.com>
2014-03-11 22:07:18 +0900

    Effekseerコンパイルスクリプト追加(Win)

--- Script/aceutils.py
+++ Script/aceutils.py
@@ -64,10 +64,41 @@ def mkdir(path):
 		os.mkdir(path)
 
 def copy(src,dst):
-	shutil.copyfile(src,dst)
+	shutil.copy(src,dst)
 
 def copytree(src,dst):
 	if not os.path.exists(dst):
 		shutil.copytree(src,dst)
 
+def editCmakeForACE(path):
+	# This script edits a cmake file for ACE.
+	# The script appends fPIC to compile on 64bit *nix OS.
+	f = open(path)
+	lines = f.read()
+	f.close()
+
+	if '# ForACE' in lines:
+		return
+
+	# to glfw
+	if 'GLFW' in lines:
+		lines = lines.replace('-Wall', '-Wall -fPIC')
+
+
+	lines = lines + "\n"
+	lines = lines + "# ForACE\n"
+	lines = lines + "if (MSVC)\n"
+	lines = lines + "else()\n"
+	lines = lines + "\tadd_definitions(-fPIC)\n"
+	lines = lines + "\tset(CMAKE_C_FLAGS \"${CMAKE_C_FLAGS} -fPIC\")\n"
+	lines = lines + "\tset(CMAKE_C_FLAGS_DEBUG \"${CMAKE_C_FLAGS_DEBUG} -fPIC\")\n"
+	lines = lines + "\tset(CMAKE_C_FLAGS_RELEASE \"${CMAKE_C_FLAGS_RELEASE} -fPIC\")\n"
+	lines = lines + "\tset(CMAKE_C_FLAGS_MINSIZEREL \"${CMAKE_C_FLAGS_MINSIZEREL} -fPIC\")\n"
+	lines = lines + "\tset(CMAKE_C_FLAGS_RELWITHDEBINFO \"${CMAKE_C_FLAGS_RELWITHDEBINFO} -fPIC\")\n"
+	lines = lines + "\tset(CMAKE_CXX_FLAGS \"${CMAKE_CXX_FLAGS} -fPIC\")\n"
+	lines = lines + "endif()\n"
+
+	f = open(path, 'w')
+	f.write(lines)
+	f.close()
 
---
+++ Script/download_effekseer.py
@@ -0,0 +1,44 @@
+
+import aceutils
+
+aceutils.cd(r'../')
+#aceutils.rm(r'master.zip')
+#aceutils.rmdir(r'Effekseer-master')
+
+#aceutils.wget(r'https://github.com/effekseer/Effekseer/archive/master.zip')
+#aceutils.unzip(r'master.zip')
+
+#aceutils.editCmakeForACE(r'Effekseer-master/Dev/Cpp/CMakeLists.txt')
+
+#aceutils.mkdir(r"effekseer_bin")
+aceutils.cd(r"effekseer_bin")
+
+if aceutils.isWin():
+	aceutils.call(r'cmake -G "Visual Studio 12" -D USE_MSVC_RUNTIME_LIBRARY_DLL:BOOL=OFF -D USE_INTERNAL_LOADER:BOOL=OFF ../Effekseer-master/Dev/Cpp/')
+	aceutils.call(r'"C:\Program Files (x86)\MSBuild\12.0\Bin\msbuild" Effekseer.sln /p:configuration=Debug')
+	aceutils.call(r'"C:\Program Files (x86)\MSBuild\12.0\Bin\msbuild" Effekseer.sln /p:configuration=Release')
+else:
+	aceutils.call(r'cmake -G "Unix Makefiles" -D USE_MSVC_RUNTIME_LIBRARY_DLL:BOOL=OFF -D USE_INTERNAL_LOADER:BOOL=OFF ../Effekseer-master/Dev/Cpp/')
+	aceutils.call(r'make')
+
+aceutils.cd(r"../")
+
+if aceutils.isWin():
+	aceutils.mkdir(r'Dev/lib/Debug')
+	aceutils.mkdir(r'Dev/lib/Release')
+	aceutils.copy(r'Effekseer-master/Dev/Cpp/Effekseer/Effekseer.h', r'Dev/include/')
+	aceutils.copy(r'Effekseer-master/Dev/Cpp/EffekseerRendererDX11/EffekseerRendererDX11.h', r'Dev/include/')
+	aceutils.copy(r'Effekseer-master/Dev/Cpp/EffekseerRendererGL/EffekseerRendererGL.h', r'Dev/include/')
+	aceutils.copy(r'effekseer_bin/Debug/Effekseer.lib', r'Dev/lib/Debug/')
+	aceutils.copy(r'effekseer_bin/Debug/EffekseerRendererDX11.lib', r'Dev/lib/Debug/')
+	aceutils.copy(r'effekseer_bin/Debug/EffekseerRendererGL.lib', r'Dev/lib/Debug/')
+	aceutils.copy(r'effekseer_bin/Release/Effekseer.lib', r'Dev/lib/Release/')
+	aceutils.copy(r'effekseer_bin/Release/EffekseerRendererDX11.lib', r'Dev/lib/Release/')
+	aceutils.copy(r'effekseer_bin/Release/EffekseerRendererGL.lib', r'Dev/lib/Release/')
+else:
+	aceutils.copy(r'Effekseer-master/Dev/Cpp/Effekseer/Effekseer.h', r'Dev/include/')
+	aceutils.copy(r'Effekseer-master/Dev/Cpp/EffekseerRendererGL/EffekseerRendererGL.h', r'Dev/include/')
+	aceutils.copy(r'effekseer_bin/libEffekseer.a', r'Dev/lib/')
+	aceutils.copy(r'effekseer_bin/libEffekseerGL.a', r'Dev/lib/')
+
+
