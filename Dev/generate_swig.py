17c38d61de8c5ab9566aed811104ecf64068714c
durswd <swda.durl@gmail.com>
2014-03-13 23:06:49 +0900

    SWIG使用時に文字コードを変換しないように

--- Dev/generate_swig.py
+++ Dev/generate_swig.py
@@ -11,17 +11,11 @@ def exec_sync( cmd ):
 	ret = p.wait()
 	print('')
 
-exec_sync( '..\\ConvertCharactorSet.exe ace_cpp/ *.cpp CP932' )
-exec_sync( '..\\ConvertCharactorSet.exe ace_cpp/ *.h CP932' )
-
-exec_sync( '..\\ConvertCharactorSet.exe unitTest_cpp_gtest/ *.cpp CP932' )
-exec_sync( '..\\ConvertCharactorSet.exe unitTest_cpp_gtest/ *.h CP932' )
-
-exec_sync( 'mkdir ace_cs\\swig' )
-exec_sync( 'swig -c++ -csharp -namespace ace.swig -dllimport ace_core -o ace_cpp\\core\\dll_cs.cxx -outdir ace_cs\\swig\\ ace_cs.i' )
-
 exec_sync( '..\\ConvertCharactorSet.exe ace_cpp/ *.cpp UTF8' )
 exec_sync( '..\\ConvertCharactorSet.exe ace_cpp/ *.h UTF8' )
 
 exec_sync( '..\\ConvertCharactorSet.exe unitTest_cpp_gtest/ *.cpp UTF8' )
 exec_sync( '..\\ConvertCharactorSet.exe unitTest_cpp_gtest/ *.h UTF8' )
+
+exec_sync( 'mkdir ace_cs\\swig' )
+exec_sync( 'swig -c++ -csharp -namespace ace.swig -dllimport ace_core -o ace_cpp\\core\\dll_cs.cxx -outdir ace_cs\\swig\\ ace_cs.i' )
