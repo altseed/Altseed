f56a10119847bd68a001b71eba59a5a0168688c7
durswd <swda.durl@gmail.com>
2014-03-15 00:19:56 +0900

    不要ファイル削除 文字コード変換追加

--- Dev/generate_swig.py
+++ Dev/generate_swig.py
@@ -1,6 +1,49 @@
 import subprocess
 import os
 import os.path
+import sys
+
+def conv(path):
+	temp = open(path,mode='rb')
+	byte = temp.read(1)
+	temp.close()
+	if b'\xef' == byte:
+		return
+
+	fin = open(path,mode='r',encoding='cp932')
+	data = fin.read()
+	fin.close()
+
+	fout = open(path,mode='w',encoding='utf-8-sig')
+	fout.write(data)
+	fout.close()
+
+def get_files(path):
+	""" get files.
+	"""
+
+	def getlistdir(path,l):
+		dirs = os.listdir(path)
+		for d in dirs:
+			newpath = os.path.join( path, d )
+			if os.path.isdir( newpath ):
+				getlistdir( newpath, l )
+			else:
+				l.append( newpath )
+	
+	ret = []
+	getlistdir( path, ret )
+	return ret
+
+
+def conv_dir(d):
+	files = get_files(d)
+	for file in files:
+		root, ext = os.path.splitext(file)
+		if not ext in ['.cpp','.h']:
+			continue
+		conv(file)
+
 
 def exec_sync( cmd ):
 	""" exec command line.
@@ -11,11 +54,9 @@ def exec_sync( cmd ):
 	ret = p.wait()
 	print('')
 
-exec_sync( '..\\ConvertCharactorSet.exe ace_cpp/ *.cpp UTF8' )
-exec_sync( '..\\ConvertCharactorSet.exe ace_cpp/ *.h UTF8' )
-
-exec_sync( '..\\ConvertCharactorSet.exe unitTest_cpp_gtest/ *.cpp UTF8' )
-exec_sync( '..\\ConvertCharactorSet.exe unitTest_cpp_gtest/ *.h UTF8' )
+conv_dir(r'ace_cpp/')
+conv_dir(r'unitTest_cpp_gtest/')
+conv_dir(r'unitTest_Engine_cpp_gtest/')
 
 exec_sync( 'mkdir ace_cs\\swig' )
 exec_sync( 'swig -c++ -csharp -namespace ace.swig -dllimport ace_core -o ace_cpp\\core\\dll_cs.cxx -outdir ace_cs\\swig\\ ace_cs.i' )
