cf5a47ea69587ea0d4d874cb61f6241acbab393a
NumAniCloud <num.ani.cloud@gmail.com>
2014-03-23 15:46:24 +0900

    フォントツール関連のコードを整理

--- Dev/ace_cpp/core/File/ace.GlyphData.h
+++ Dev/ace_cpp/core/File/ace.GlyphData.h
@@ -1,6 +1,8 @@
 ﻿#pragma once
 #include "../common/ace.common.Base.h"
 #include "../common/Math/ace.RectI.h"
+#include "../common/Utility/ace.BinaryWriter.h"
+#include "../common/Utility/ace.BinaryReader.h"
 
 namespace ace
 {
@@ -11,8 +13,14 @@ namespace ace
 		int m_sheetNum;
 		RectI m_src;
 
+		GlyphData();
+
 	public:
 		GlyphData(achar charactor, int sheetNum, RectI src);
+
+		void Push(BinaryWriter& writer);
+
+		static GlyphData Get(BinaryReader& reader, achar charactor);
 
 		achar GetCharactor() const
 		{
