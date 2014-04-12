fce62eb4cd3adadc9916025f470ad19f445be201
NumAniCloud <num.ani.cloud@gmail.com>
2014-04-12 12:43:34 +0900

    テストを実行するための変更

--- Dev/unitTest_Engine_cpp_gtest/main.cpp
+++ Dev/unitTest_Engine_cpp_gtest/main.cpp
@@ -49,8 +49,8 @@ int main(int argc, char **argv)
 #endif
 	::testing::InitGoogleTest(&argc, argv);
 
-	//Graphics_SimpleMesh_(true);
-	//return 0;
+	Graphics_SimpleMesh_(true);
+	return 0;
 
 	//TestCamera(true);
 	//TestCamera(false);
