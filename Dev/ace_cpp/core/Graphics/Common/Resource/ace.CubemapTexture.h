55378ef7163a5ac6336f2de5abc467d77abb1c32
durswd <swda.durl@gmail.com>
2014-04-24 23:07:43 +0900

    キューブマップのファイルだけ追加

---
+++ Dev/ace_cpp/core/Graphics/Common/Resource/ace.CubemapTexture.h
@@ -0,0 +1,19 @@
+
+#pragma once
+
+#include <ace.common.Base.h>
+#include "../../../ace.ReferenceObject.h"
+
+namespace ace
+{
+	class CubemapTexture
+		: public IReference
+	{
+	protected:
+		CubemapTexture() {}
+		virtual ~CubemapTexture() {}
+
+	public:
+
+	};
+}
