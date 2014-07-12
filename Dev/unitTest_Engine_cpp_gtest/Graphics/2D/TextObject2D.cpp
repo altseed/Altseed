6e2516399beb451dce4afb3d89f554f08b5a85f6
durswd <swda.durl@gmail.com>
2014-07-12 23:48:28 +0900

    フォント周りのテスト修正

--- Dev/unitTest_Engine_cpp_gtest/Graphics/2D/TextObject2D.cpp
+++ Dev/unitTest_Engine_cpp_gtest/Graphics/2D/TextObject2D.cpp
@@ -20,26 +20,21 @@ protected:
 		auto scene = make_shared<Scene>();
 		auto layer = make_shared<Layer2D>();
 		auto object = make_shared<TextObject2D>();
-		auto back = make_shared<TextureObject2D>();
 
 		ASSERT_NE(scene, nullptr);
 		ASSERT_NE(layer, nullptr);
 		ASSERT_NE(object, nullptr);
-		ASSERT_NE(back, nullptr);
 
 		ace::Engine::ChangeScene(scene);
 		scene->AddLayer(layer);
 
 		auto g = ace::Engine::GetGraphics();
 
-		layer->AddObject(back);
-		back->SetTexture(g->CreateTexture2D(ace::ToAString("Data/Text/back.png").c_str()));
-
 		layer->AddObject(object);
-		auto font = g->CreateFont(ace::ToAString("Data/Text/Nac0711.aff").c_str());
+		auto font = g->CreateFont(ace::ToAString("Data/Font/Nac0711.aff").c_str());
 
 		object->SetFont(font);
-		object->SetText(ToAString("DPは依存関係\nプロパティ"));
+		object->SetText(ToAString("DependencyPropertyは\n依存関係プロパティのこと"));
 
 		//object->SetCenterPosition(Vector2DF(128, 128));
 		object->SetPosition(Vector2DF(0, 0));
