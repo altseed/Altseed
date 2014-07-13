05c0bc943cee60f96c303b186f4584890567a00b
NumAniCloud <num.ani.cloud@gmail.com>
2014-07-13 13:45:09 +0900

    フォント生成ツールのWPF版を作成

--- Dev/FontGenerator/FontGenerator.Model/GenerationConfig.cs
+++ Dev/FontGenerator/FontGenerator.Model/GenerationConfig.cs
@@ -1,17 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace FontGenerator.Model
 {
+	[DataContract]
 	public class GenerationConfig
 	{
+		[DataMember]
 		public string FontPath { get; set; }
+		[DataMember]
 		public string TextPath { get; set; }
+		[DataMember]
 		public string ExportPath { get; set; }
+		[DataMember]
+		public string SheetName { get; set; }
+		[DataMember]
 		public int FontSize { get; set; }
-		public Color FontColor { get; set; }
+		[DataMember]
+		public int TextureSize { get; set; }
+		[DataMember]
+		public Color FontColor;
 	}
 }
--- Dev/FontGenerator/FontGenerator.Model/Generator.cs
+++ Dev/FontGenerator/FontGenerator.Model/Generator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,11 +12,27 @@ namespace FontGenerator.Model
 	{
 		public static async Task GenerateAsync(GenerationConfig config)
 		{
+			if(!File.Exists(config.FontPath))
+			{
+				throw new FileNotFoundException("指定されたフォントファイルは存在しません。", config.FontPath);
+			}
+
+			if(!File.Exists(config.TextPath))
+			{
+				throw new FileNotFoundException("指定されたテキストファイルは存在しません。", config.TextPath);
+			}
+
+			if(!Directory.Exists(config.ExportPath))
+			{
+				throw new FileNotFoundException("指定され出力先ディレクトリは存在しません。", config.ExportPath);
+			}
+
 			var gen = new DLL();
 			gen.SetFontName(config.FontPath);
 			gen.SetTextFilePath(config.TextPath);
-			gen.SetExportPath(config.ExportPath);
+			gen.SetExportPath(Path.Combine(config.ExportPath, config.SheetName));
 			gen.SetFontSize(config.FontSize);
+			gen.SetSheetSize(config.TextureSize);
 
 			var c = config.FontColor;
 			gen.SetFontColor(c.Red, c.Green, c.Blue, c.Alpha);
