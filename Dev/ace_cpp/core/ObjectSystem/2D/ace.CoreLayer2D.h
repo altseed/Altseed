63e6dfd0da0887797c70b48c55063551c8530fd5
c_plus_c <dtm.music.producer@gmail.com>
2014-07-19 06:37:20 +0900

    Layer2Dにテクスチャ追加描画関数追加(C++)

--- Dev/ace_cpp/core/ObjectSystem/2D/ace.CoreLayer2D.h
+++ Dev/ace_cpp/core/ObjectSystem/2D/ace.CoreLayer2D.h
@@ -29,6 +29,11 @@ namespace ace
 		virtual void AddObject(ObjectPtr object2D) = 0;
 		virtual void RemoveObject(ObjectPtr object2D) = 0;
 
+		virtual void DrawSpriteAdditionally(Vector2DF pos1, Vector2DF pos2, Vector2DF pos3, Vector2DF pos4,
+			Color col1, Color col2, Color col3, Color col4,
+			Vector2DF uv1, Vector2DF uv2, Vector2DF uv3, Vector2DF uv4,
+			Texture2D* texture, AlphaBlend alphaBlend, int32_t priority) = 0;
+
 #if !SWIG
 		virtual Renderer2D* GetRenderer() const = 0;
 		virtual Graphics* GetGraphicsImp() = 0;
