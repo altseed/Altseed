507d73677778c9202a600a1375bb4a0e1ca49226
isishizuka <isishizuka@gmail.com>
2014-12-27 01:06:37 +0900

    ファイル周りとりあえずの実装

--- Dev/ace_cpp/core/IO/ace.File_Imp.h
+++ Dev/ace_cpp/core/IO/ace.File_Imp.h
@@ -47,6 +47,8 @@ namespace ace
 		void SetRootDirectories(_InIt first, _InIt end);
 		StreamFile* CreateStreamFileDirectly(const astring& normalizedPath);
 
+		bool ConsistOf(const std::vector<astring>& vec, const astring& element) const { for (const auto& v : vec) { if (v == element) return true; } return false; }
+
 	public:
 		static File_Imp* Create() { return new File_Imp(); };
 
--- Dev/ace_cpp/core/IO/ace.PackFile_Imp.h
+++ Dev/ace_cpp/core/IO/ace.PackFile_Imp.h
@@ -2,6 +2,7 @@
 
 #include "ace.PackFile.Header_Imp.h"
 #include "ace.Path_Imp.h"
+#include "ace.BaseFile_Imp.h"
 #include <memory>
 #include <unordered_map>
 #include <cassert>
@@ -13,9 +14,12 @@ namespace ace
 	private:
 		std::shared_ptr<TopHeader> m_TopHeader;
 		std::unordered_map<astring, std::shared_ptr<InternalHeader>> m_InternalHeaderDictionaly;
-		
+		std::shared_ptr<BaseFile_Imp> m_RawFile;
+
 	public:
-		PackFile_Imp(const astring& path);
+		decltype(m_RawFile) RawFile() { return m_RawFile; }
+
+		PackFile_Imp(std::shared_ptr<BaseFile_Imp> packedFile);
 		bool HaveFile(const astring& path);
 		std::shared_ptr<TopHeader> GetTopHeader();
 		std::shared_ptr<InternalHeader> GetInternalHeader(const astring& path);
