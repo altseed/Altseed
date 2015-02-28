a549c391ca4ebc538980d8c17054021977b5bbc1
c_plus_c <dtm.music.producer@gmail.com>
2015-03-01 00:10:06 +0900

    カメラのテストをちょっとまともに #80

--- Sample/sample_cpp/Graphics/2D/CameraObject2D.cpp
+++ Sample/sample_cpp/Graphics/2D/CameraObject2D.cpp
@@ -23,7 +23,7 @@ void CameraObject2D()
 
 	{
 		// テクスチャを生成する
-		auto tex0 = ace::Engine::GetGraphics()->CreateTexture2D(ace::ToAString("Data/Texture/Sample2.png").c_str());
+		auto tex0 = ace::Engine::GetGraphics()->CreateTexture2D(ace::ToAString("Data/Texture/Sample1.png").c_str());
 
 		//TextureObject2Dを生成する
 		auto obj0 = std::make_shared<ace::TextureObject2D>();
@@ -32,22 +32,34 @@ void CameraObject2D()
 		obj0->SetTexture(tex0);
 
 		//描画位置を指定する
-		obj0->SetPosition(ace::Vector2DF(100, 100));
+		obj0->SetCenterPosition(ace::Vector2DF(256, 256));
+		obj0->SetPosition(ace::Vector2DF(320, 240));
+		obj0->SetScale(ace::Vector2DF(0.5f, 0.5f));
 
 		//レイヤーへ追加する
 		layer->AddObject(obj0);
 	}
 
+	//画面全体を写すカメラ(オブジェクトをそのまま描画)
 	auto camera1 = std::make_shared<ace::CameraObject2D>();
-	camera1->SetSrc(ace::RectI(0, 0, 200, 200));
-	camera1->SetDst(ace::RectI(0, 0, 200, 200));
+	camera1->SetSrc(ace::RectI(0, 0, 640, 480));
+	camera1->SetDst(ace::RectI(0, 0, 640, 480));
 
 	layer->AddObject(camera1);
 
+	//ポインタ周りを拡大して表示するカメラ
+	auto camera2 = std::make_shared<ace::CameraObject2D>();
+
+	layer->AddObject(camera2);
 
 	// aceが進行可能かチェックする。
 	while (ace::Engine::DoEvents())
 	{
+		auto pos = ace::Engine::GetMouse()->GetPosition();
+
+		camera2->SetDst(ace::RectI(pos.X - 50, pos.Y - 50, 100, 100));
+		camera2->SetSrc(ace::RectI(pos.X - 25, pos.Y - 25, 50, 50));
+
 		// aceを更新する。
 		ace::Engine::Update();
 	}
