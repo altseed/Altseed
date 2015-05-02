678680d223c7e9886e2782198a262893762052b9
c_plus_c <dtm.music.producer@gmail.com>
2015-05-02 22:25:02 +0900

    図形描画のnullチェック追加

---
+++ Dev/unitTest_Engine_cs/Graphics/2D/AdditionallyDraw.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace unitTest_Engine_cs.Graphics._2D
+{
+
+    class AdditionallyDraw : EngineTest
+    {
+        class CustomObject : ace.TextureObject2D
+        {
+            public CustomObject()
+                : base()
+            {
+
+            }
+
+            protected override void OnDrawAdditionally()
+            {
+                DrawCircleAdditionally(new ace.Vector2DF(320, 800 + 240), 1600, 1570, new ace.Color(255, 255, 255, 255), 32, 0, null, ace.AlphaBlendMode.Add, 0);
+            }
+        }
+
+        public AdditionallyDraw()
+            : base(60)
+        {
+        }
+
+        protected override void OnStart()
+        {
+            var scene = new ace.Scene();
+
+
+            var layer = new ace.Layer2D();
+
+            scene.AddLayer(layer);
+
+            var obj = new CustomObject();
+            layer.AddObject(obj);
+
+            ace.Engine.ChangeScene(scene);
+        }
+
+        protected override void OnUpdating()
+        {
+
+        }
+    }
+}
