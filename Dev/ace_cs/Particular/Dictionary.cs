62ef9498480548892b1817aba0f8b67fcb834f1c
durswd <swda.durl@gmail.com>
2015-05-28 01:41:17 +0900

    Dictionaryをラップした

---
+++ Dev/ace_cs/Particular/Dictionary.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ace.Particular
+{
+	class Dictionary
+	{
+		public static void Set<TKey, TValue>(Dictionary<TKey,TValue> d, TKey key, TValue value)
+		{
+			d[key] = value;
+		}
+
+		public static TValue Get<TKey, TValue>(Dictionary<TKey,TValue> d, TKey key)
+		{
+			return d[key];
+		}
+	}
+}
