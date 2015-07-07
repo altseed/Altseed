5fbce01e06c4e9a4323563141163d1d0781055d4
c_plus_c <dtm.music.producer@gmail.com>
2015-07-07 23:46:57 +0900

    入力関連のサンプル修正中

--- Sample/BasicSample/sample_cpp/Input/Mouse.cpp
+++ Sample/BasicSample/sample_cpp/Input/Mouse.cpp
@@ -1,11 +1,30 @@
-﻿
-#include <Altseed.h>
+﻿#include <Altseed.h>
 
-// Mouseのサンプル。マウスによる入力を取得しています。
+/**
+@brief	Mouseのサンプル。マウスによる入力を取得しています。
+*/
 void Mouse()
 {
 	//Altseedを初期化する。
 	asd::Engine::Initialize(asd::ToAString("Mouse").c_str(), 640, 480, asd::EngineOption());
+
+	auto font = asd::Engine::GetGraphics()->CreateDynamicFont(asd::ToAString("").c_str(), 20, asd::Color(255, 255, 255, 255), 1, asd::Color(0, 0, 0, 255));
+
+	auto mousePosText = std::make_shared<asd::TextObject2D>();
+	mousePosText->SetPosition(asd::Vector2DF(100, 100));
+	mousePosText->SetFont(font);
+
+	auto wheelStateText = std::make_shared<asd::TextObject2D>();
+	wheelStateText->SetPosition(asd::Vector2DF(100, 300));
+	wheelStateText->SetFont(font);
+
+	auto leftButtonStateText = std::make_shared<asd::TextObject2D>();
+	leftButtonStateText->SetPosition(asd::Vector2DF(100, 200));
+	leftButtonStateText->SetFont(font);
+
+	asd::Engine::AddObject2D(mousePosText);
+	asd::Engine::AddObject2D(wheelStateText);
+	asd::Engine::AddObject2D(leftButtonStateText);
 
 	// Altseedのウインドウが閉じられていないか確認する。
 	while (asd::Engine::DoEvents())
@@ -17,7 +36,7 @@ void Mouse()
 		{
 			asd::Vector2DF v = mouse->GetPosition();
 
-			printf("Cursor Position:(%f,%f) ", v.X, v.Y);
+			mousePosText->SetText(asd::ToAString(("Cursor Position : (" + std::to_string((int)(v.X)) + "," + std::to_string((int)(v.Y)) + ")").c_str()).c_str());
 		}
 
 		// マウスの中央クリック状態を取得して表示する。
@@ -25,25 +44,25 @@ void Mouse()
 			switch (mouse->GetMiddleButton()->GetButtonState())
 			{
 			case asd::MouseButtonState::Free: //前フレームと本フレームで非押下
-				printf("Middle Free");
+				leftButtonStateText->SetText(asd::ToAString("Left Button : Free").c_str());
 				break;
 			case asd::MouseButtonState::Hold: //前フレームと本フレームで押下
-				printf("Middle Hold");
+				leftButtonStateText->SetText(asd::ToAString("Left Button : Hold").c_str());
 				break;
 			case asd::MouseButtonState::Release: //前フレームで押下、本フレームで非押下
-				printf("Middle Release");
+				leftButtonStateText->SetText(asd::ToAString("Left Button : Release").c_str());
 				break;
 			case asd::MouseButtonState::Push: //前フレームで非押下、本フレームで押下
-				printf("Middle Push");
+				leftButtonStateText->SetText(asd::ToAString("Left Button : Push").c_str());
 				break;
 			}
 		}
 
 		// マウスのホイールの回転状態を取得して表示する。
 		{
-			printf(" Wheel : %f\n", mouse->GetMiddleButton()->GetRotation());
+			wheelStateText->SetText(asd::ToAString(("Wheel : " + std::to_string(mouse->GetMiddleButton()->GetRotation())).c_str()).c_str());
 		}
-
+		
 		// Altseedを更新する。
 		asd::Engine::Update();
 	}
