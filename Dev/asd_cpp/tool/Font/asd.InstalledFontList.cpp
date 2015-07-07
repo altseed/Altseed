35c3b9b3529b0d8016d4e262d9f8f6c97ca65fe7
durswd <swda.durl@gmail.com>
2015-07-07 21:45:34 +0900

    フォント名を英語名で取得できるように変更

--- Dev/asd_cpp/tool/Font/asd.InstalledFontList.cpp
+++ Dev/asd_cpp/tool/Font/asd.InstalledFontList.cpp
@@ -1,9 +1,13 @@
 ﻿#include "asd.InstalledFontList.h"
 
 #ifdef _WIN32
+#include <filesystem>
 #include <Shlwapi.h>
 #pragma comment(lib,"Shlwapi.lib")
 #endif
+
+#include <ft2build.h>
+#include FT_SFNT_NAMES_H
 
 namespace asd
 {
@@ -16,6 +20,41 @@ namespace asd
 		isLoaded = true;
 
 #ifdef _WIN32
+
+		std::vector<std::string> paths;
+		std::tr2::sys::path fontRoot("C:\\Windows\\Fonts\\");
+		
+		std::for_each(
+			std::tr2::sys::directory_iterator(fontRoot), 
+			std::tr2::sys::directory_iterator(),
+			[&paths](const std::tr2::sys::path& p) {
+			if (std::tr2::sys::is_regular_file(p))
+			{
+				paths.push_back(p);
+			}});
+
+		FT_Library library = nullptr;
+		auto error = FT_Init_FreeType(&library);
+
+		for (auto& path : paths)
+		{
+			FT_Face face = nullptr;
+			FT_New_Face(library, path.c_str(), 0, &face);
+			if (face == nullptr) continue;
+
+			Font font;
+			font.Name = ToAString(face->family_name);
+			font.Path = ToAString(path.c_str());
+
+			Fonts.push_back(font);
+
+			FT_Done_Face(face);
+		}
+
+		FT_Done_FreeType(library);
+
+		// レジストリだとフルネームが長い
+		/*
 		HKEY fontKey;
 
 		// レジストリを開く
@@ -58,6 +97,8 @@ namespace asd
 
 		// 終了
 		RegCloseKey(fontKey);
+		*/
+
 #elif defined __APPLE__
 		Load_Mac();
 #endif
