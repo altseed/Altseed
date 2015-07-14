7245d29a3aa44dba9d3f573eda5c0388729ad97d
c_plus_c <dtm.music.producer@gmail.com>
2015-07-14 22:29:43 +0900

    Revert "C#版をIEnumerableからListに変更"
    This reverts commit 5764577c19f314cb4099ab1e90c4b17da66e7aed.

--- Dev/asd_cs/ObjectSystem/2D/MapObject2D.cs
+++ Dev/asd_cs/ObjectSystem/2D/MapObject2D.cs
@@ -87,7 +87,7 @@ namespace asd
         /// このオブジェクトが保有しているチップを全て取得する。
         /// </summary>
         /// <returns></returns>
-        public List<Chip2D> Chips
+        public IEnumerable<Chip2D> Chips
         {
             get { return chips; }
         }
