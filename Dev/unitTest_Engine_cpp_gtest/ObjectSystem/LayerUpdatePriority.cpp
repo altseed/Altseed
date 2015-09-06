5a806847ec6b925d07dd9d90271f252e61a20e7e
ryogo.yoshimura <ryogo.yoshimura@gmail.com>
2015-09-07 00:40:21 +0900

    テストケースを自動化

--- Dev/unitTest_Engine_cpp_gtest/ObjectSystem/LayerUpdatePriority.cpp
+++ Dev/unitTest_Engine_cpp_gtest/ObjectSystem/LayerUpdatePriority.cpp
@@ -6,47 +6,66 @@
 using namespace std;
 using namespace asd;
 
+class ObjectSystem_LayerUpdatePriority;
+
 class MessageLayer : public Layer2D
 {
 private:
+	ObjectSystem_LayerUpdatePriority *m_parent;
 	astring m_message;
 
 protected:
-	void OnUpdating() override
-	{
-		wprintf((m_message + L"\n").c_str());
-		Vanish();
-	}
+	void OnUpdating() override;
 
 public:
-	MessageLayer(astring message)
-		: m_message(message)
+	MessageLayer(ObjectSystem_LayerUpdatePriority *parent, astring message)
+		: m_parent(parent), m_message(message)
 	{
 	}
 };
 
 class ObjectSystem_LayerUpdatePriority : public EngineTest
 {
+private:
+	std::vector<astring> m_messages;
+
 public:
 	ObjectSystem_LayerUpdatePriority(bool isOpenGLMode)
 		: EngineTest(asd::ToAString("LayerUpdatePriority"), isOpenGLMode, 60)
 	{
 	}
 
+	void AddMessage(const astring &message) {
+		m_messages.push_back(message);
+	}
+
 protected:
 	void OnStart()
 	{
 		auto scene = make_shared<Scene>();
-		auto layer1 = make_shared<MessageLayer>(ToAString("Layer1"));
+		auto layer1 = make_shared<MessageLayer>(this, ToAString("Layer1"));
 		layer1->SetUpdatePriority(1);
 
-		auto layer2 = make_shared<MessageLayer>(ToAString("Layer2"));
+		auto layer2 = make_shared<MessageLayer>(this, ToAString("Layer2"));
 		layer2->SetUpdatePriority(0);
 
 		scene->AddLayer(layer1);
 		scene->AddLayer(layer2);
 		asd::Engine::ChangeScene(scene);
 	}
+
+	void OnFinish()
+	{
+		ASSERT_EQ(2, m_messages.size());
+		ASSERT_EQ(ToAString("Layer2"), m_messages[0]);
+		ASSERT_EQ(ToAString("Layer1"), m_messages[1]);
+	}
 };
+
+void MessageLayer::OnUpdating()
+{
+	m_parent->AddMessage(m_message);
+	Vanish();
+}
 
 ENGINE_TEST(ObjectSystem, LayerUpdatePriority)
--- Dev/unitTest_Engine_cpp_gtest/ObjectSystem/ObjectsUpdatePriority.cpp
+++ Dev/unitTest_Engine_cpp_gtest/ObjectSystem/ObjectsUpdatePriority.cpp
@@ -6,21 +6,20 @@
 using namespace std;
 using namespace asd;
 
+class ObjectSystem_ObjectsUpdatePriority;
+
 class MessageObject : public TextureObject2D
 {
 private:
+	ObjectSystem_ObjectsUpdatePriority *m_parent;
 	astring m_message;
 
 protected:
-	void OnUpdate() override
-	{
-		wprintf((m_message + L"\n").c_str());
-		Vanish();
-	}
+	void OnUpdate() override;
 
 public:
-	MessageObject(astring message)
-		: m_message(message)
+	MessageObject(ObjectSystem_ObjectsUpdatePriority *parent, astring message)
+		: m_parent(parent), m_message(message)
 	{
 		auto texture = asd::Engine::GetGraphics()->CreateTexture2D(asd::ToAString("Data/Texture/Cloud1.png").c_str());
 		SetTexture(texture);
@@ -29,32 +28,54 @@ public:
 
 class ObjectSystem_ObjectsUpdatePriority : public EngineTest
 {
+private:
+	std::vector<astring> m_messages;
+
 public:
 	ObjectSystem_ObjectsUpdatePriority(bool isOpenGLMode)
 		: EngineTest(asd::ToAString("ObjectsUpdatePriority"), isOpenGLMode, 60)
 	{
 	}
 
+	void AddMessage(const astring &message)
+	{
+		m_messages.push_back(message);
+	}
+
 protected:
 	void OnStart()
 	{
-		auto obj1 = make_shared<MessageObject>(ToAString("object1"));
+		auto obj1 = make_shared<MessageObject>(this, ToAString("object1"));
 		obj1->SetUpdatePriority(2);
 		asd::Engine::AddObject2D(obj1);
 
-		auto obj2 = make_shared<MessageObject>(ToAString("object2"));
+		auto obj2 = make_shared<MessageObject>(this, ToAString("object2"));
 		obj2->SetUpdatePriority(1);
 		asd::Engine::AddObject2D(obj2);
 
-		auto obj3 = make_shared<MessageObject>(ToAString("object3"));
+		auto obj3 = make_shared<MessageObject>(this, ToAString("object3"));
 		asd::Engine::AddObject2D(obj3);
 		obj3->SetUpdatePriority(0);
 
-		auto obj4 = make_shared<MessageObject>(ToAString("object4"));
+		auto obj4 = make_shared<MessageObject>(this, ToAString("object4"));
 		asd::Engine::AddObject2D(obj4);
 		asd::Engine::RemoveObject2D(obj4);
 		obj4->SetUpdatePriority(0);
 	}
+
+	void OnFinish()
+	{
+		ASSERT_EQ(3, m_messages.size());
+		ASSERT_EQ(ToAString("object3"), m_messages[0]);
+		ASSERT_EQ(ToAString("object2"), m_messages[1]);
+		ASSERT_EQ(ToAString("object1"), m_messages[2]);
+	}
 };
+
+void MessageObject::OnUpdate()
+{
+	m_parent->AddMessage(m_message);
+	Vanish();
+}
 
 ENGINE_TEST(ObjectSystem, ObjectsUpdatePriority)
