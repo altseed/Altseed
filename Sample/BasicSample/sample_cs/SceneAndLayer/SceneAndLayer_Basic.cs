4fd384de360b75e39ff3d0bee9b9a19ff12148d6
NumAniCloud <num.ani.cloud@gmail.com>
2015-11-23 00:09:21 +0900

    C#サンプルで不要なusingを一部削除

--- Sample/BasicSample/sample_cs/SceneAndLayer/SceneAndLayer_Basic.cs
+++ Sample/BasicSample/sample_cs/SceneAndLayer/SceneAndLayer_Basic.cs
@@ -1,53 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace sample_cs
+﻿namespace sample_cs
 {
-    class SceneAndLayer_Basic : ISample
-    {
-        // シーンとレイヤーを手動で生成する。
-        public void Run()
-        {
-            // Altseedを初期化する。
-            asd.Engine.Initialize("SceneAndLayer_Basic", 640, 480, new asd.EngineOption());
+	class SceneAndLayer_Basic : ISample
+	{
+		// シーンとレイヤーを手動で生成する。
+		public void Run()
+		{
+			// Altseedを初期化する。
+			asd.Engine.Initialize("SceneAndLayer_Basic", 640, 480, new asd.EngineOption());
 
-            // 画像を読み込む。
-            var texture = asd.Engine.Graphics.CreateTexture2D("Data/Texture/Picture1.png");
+			// 画像を読み込む。
+			var texture = asd.Engine.Graphics.CreateTexture2D("Data/Texture/Picture1.png");
 
-            // シーンクラスのインスタンスを生成する。
-            var scene = new asd.Scene();
+			// シーンクラスのインスタンスを生成する。
+			var scene = new asd.Scene();
 
-            // レイヤークラスのインスタンスを生成する。
-            var layer = new asd.Layer2D();
+			// レイヤークラスのインスタンスを生成する。
+			var layer = new asd.Layer2D();
 
-            //画像描画オブジェクトのインスタンスを生成する。
-            var obj = new asd.TextureObject2D();
+			//画像描画オブジェクトのインスタンスを生成する。
+			var obj = new asd.TextureObject2D();
 
-            // オブジェクトの位置とテクスチャを設定する。
-            obj.Position = new asd.Vector2DF(50, 50);
-            obj.Texture = texture;
-            
-            // 描画するシーンを指定する。
-            asd.Engine.ChangeScene(scene);
+			// オブジェクトの位置とテクスチャを設定する。
+			obj.Position = new asd.Vector2DF(50, 50);
+			obj.Texture = texture;
+			
+			// 描画するシーンを指定する。
+			asd.Engine.ChangeScene(scene);
 
-            // 描画するレイヤーをシーンに追加する。
-            scene.AddLayer(layer);
+			// 描画するレイヤーをシーンに追加する。
+			scene.AddLayer(layer);
 
-            // 描画するオブジェクトをレイヤーに追加する。
-            layer.AddObject(obj);
-            
-            // Altseedのウインドウが閉じられていないか確認する。
-            while (asd.Engine.DoEvents())
-            {
-                // Altseedを更新する。
-                asd.Engine.Update();
-            }
-            
-            // Altseedを終了する。
-            asd.Engine.Terminate();
-        }
-    }
+			// 描画するオブジェクトをレイヤーに追加する。
+			layer.AddObject(obj);
+			
+			// Altseedのウインドウが閉じられていないか確認する。
+			while (asd.Engine.DoEvents())
+			{
+				// Altseedを更新する。
+				asd.Engine.Update();
+			}
+			
+			// Altseedを終了する。
+			asd.Engine.Terminate();
+		}
+	}
 }
--- Sample/BasicSample/sample_cs/Sound/Sound_BGM.cs
+++ Sample/BasicSample/sample_cs/Sound/Sound_BGM.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace sample_cs
+﻿namespace sample_cs
 {
 	class Sound_BGM : ISample
 	{
