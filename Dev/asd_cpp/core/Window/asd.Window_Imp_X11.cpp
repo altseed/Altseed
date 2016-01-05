af8152c47a68fd995bb48e1224da481ffdd219d6
neofuji <shizuruma@outlook.jp>
2016-01-06 08:08:41 +0900

    asd.Window_Imp_*::CallbackOnFocus 関数に null チェックを追加

--- Dev/asd_cpp/core/Window/asd.Window_Imp_X11.cpp
+++ Dev/asd_cpp/core/Window/asd.Window_Imp_X11.cpp
@@ -15,7 +15,12 @@ void Window_Imp_X11::CallbackOnFocus(GLFWwindow* window, int b)
 	auto w = (Window_Imp_X11*) glfwGetWindowUserPointer(window);
 	if (b == GL_TRUE)
 	{
-		w->OnFocused();
+		auto onFocused = w->OnFocused;
+
+		if (onFocused)
+		{
+			onFocused();
+		}
 	}
 	else
 	{
