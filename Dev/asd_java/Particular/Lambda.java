afd8a50fe97c8bf251128f6e77c72e2c42d2028b
durswd <swda.durl@gmail.com>
2016-06-07 01:35:55 +0900

    Javaのコードのコンパイルが通るように

--- Dev/asd_java/Particular/Lambda.java
+++ Dev/asd_java/Particular/Lambda.java
@@ -1,6 +1,9 @@
 package asd.Particular;
 
 import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class Lambda
 {
@@ -22,26 +25,49 @@ public class Lambda
 
 	public static java.lang.Iterable<asd.Object2D> FilterDeadObject(java.util.ArrayList<asd.Object2D> objects)
 	{
-		return objects.stream().filter(x -> x.IsAlive)::iterator;
+		return objects.stream().filter(x -> x.getIsAlive())::iterator;
 	}
 
 	public static void RemoveDead(java.util.ArrayList<asd.Object2D> objects)
 	{
-		objects.RemoveAll(x -> !x.IsAlive);
+		java.util.ArrayList<asd.Object2D> removed = new java.util.ArrayList<asd.Object2D>();
+		for(asd.Object2D o : objects)
+		{
+			if(!o.getIsAlive())
+			{
+				removed.add(o);
+			}
+		}
+		objects.removeAll(removed);
 	}
 
 	public static void SortByUpdatePriority(java.util.ArrayList<asd.Layer> layers)
 	{
-		layers.Sort((x, y) -> x.UpdatePriority - y.UpdatePriority);
+		Collections.sort(layers,
+			new Comparator<asd.Layer>()
+			{
+				public int compare( asd.Layer x, asd.Layer y ){ return x.getUpdatePriority() - y.getUpdatePriority(); }
+			}
+		);
+
+		//layers.Sort((x, y) -> x.UpdatePriority - y.UpdatePriority);
 	}
 
 	public static <TContent> java.lang.Iterable<TContent> ToLinear(java.util.TreeMap<Integer, java.util.LinkedList<TContent>> contents)
 	{
-		return contents.SelectMany(x -> x.Value);
+		return contents.values().stream().flatMap(v->v.stream())::iterator;
+		//return contents.SelectMany(x -> x.Value);
 	}
 
 	public static void SortByDrawingPriority(java.util.ArrayList<asd.Layer> layersToDraw)
 	{
-		layersToDraw.Sort((x, y) -> x.DrawingPriority - y.DrawingPriority);
+		Collections.sort(layersToDraw,
+			new Comparator<asd.Layer>()
+			{
+				public int compare( asd.Layer x, asd.Layer y ){ return x.getDrawingPriority() - y.getDrawingPriority(); }
+			}
+		);
+
+		//layersToDraw.Sort((x, y) -> x.DrawingPriority - y.DrawingPriority);
 	}
 }
