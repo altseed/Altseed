7766e6abf26dc587985c8d6f6decc3e7f322a54a
Asato Imanishi <dtm.music.producer@gmail.com>
2016-06-29 23:07:40 +0900

    ApplicationSampleから?を削除

--- Sample/ApplicationSample/sample_cs/Browser/SampleBrowserLayer.cs
+++ Sample/ApplicationSample/sample_cs/Browser/SampleBrowserLayer.cs
@@ -7,95 +7,110 @@ using asd;
 
 namespace sample_cs
 {
-	class SampleBrowserLayer : Layer2D
-	{
-		public static readonly int Columns = 3;
-		public static readonly Vector2DF ItemOffset = new Vector2DF(632 / Columns, 150);
+    class SampleBrowserLayer : Layer2D
+    {
+        public static readonly int Columns = 3;
+        public static readonly Vector2DF ItemOffset = new Vector2DF(632 / Columns, 150);
 
-		private List<SampleItem> items;
-		private CameraObject2D camera;
-		private SampleItem activeItem;
+        private List<SampleItem> items;
+        private CameraObject2D camera;
+        private SampleItem activeItem;
 
         public event Action<ISample> SelectionChanged;
-		public event Action<ISample> OnDecide;
+        public event Action<ISample> OnDecide;
         public RectF CameraArea
         {
             get { return camera.Src.ToF(); }
         }
         public float TotalHeight { get; private set; }
 
-		public SampleBrowserLayer(ISample[] samples)
-		{
+        public SampleBrowserLayer(ISample[] samples)
+        {
             Name = "BrowserLayer";
-			items = new List<SampleItem>();
+            items = new List<SampleItem>();
 
-			var font = Engine.Graphics.CreateDynamicFont("", 12, new Color(255, 255, 255, 255), 1, new Color(0, 0, 0, 255));
-			int index = 0;
-			foreach(var sample in samples)
-			{
-				var item = new SampleItem(sample, font);
-				var x = ItemOffset.X * (index % Columns) + 8;
-				var y = ItemOffset.Y * (index / Columns) + 8 + 20;
-				item.Position = new Vector2DF(x, y);
-				AddObject(item);
-				items.Add(item);
+            var font = Engine.Graphics.CreateDynamicFont("", 12, new Color(255, 255, 255, 255), 1, new Color(0, 0, 0, 255));
+            int index = 0;
+            foreach (var sample in samples)
+            {
+                var item = new SampleItem(sample, font);
+                var x = ItemOffset.X * (index % Columns) + 8;
+                var y = ItemOffset.Y * (index / Columns) + 8 + 20;
+                item.Position = new Vector2DF(x, y);
+                AddObject(item);
+                items.Add(item);
 
-				++index;
-			}
+                ++index;
+            }
 
-            var isThereJut = index % Columns == 0 ? 0 : 1;
-            TotalHeight = ItemOffset.Y * (index / Columns + isThereJut) + 8 + 20;
+            var isThereJut = index % Columns == 0;
+
+            if (isThereJut)
+            {
+                TotalHeight = ItemOffset.Y * (index / Columns) + 8 + 20;
+            }
+            else
+            {
+                TotalHeight = ItemOffset.Y * (index / Columns + 1) + 8 + 20;
+            }
 
             camera = new CameraObject2D()
-			{
-				Src = new RectI(0, 0, 640, 480),
-				Dst = new RectI(0, 0, 640, 480),
-			};
-			AddObject(camera);
-		}
+            {
+                Src = new RectI(0, 0, 640, 480),
+                Dst = new RectI(0, 0, 640, 480),
+            };
+            AddObject(camera);
+        }
 
-		protected override void OnUpdated()
-		{
-			var rows = (items.Count / Columns) + (items.Count % Columns == 0 ? 0 : 1);
+        protected override void OnUpdated()
+        {
+            var rows = items.Count / Columns;
+
+            if (items.Count % Columns != 0)
+            {
+                rows++;
+            }
+
+
             var y = camera.Src.Y - Engine.Mouse.MiddleButton.WheelRotation * 30;
-			y = Math.Max(0, y);
-			y = Math.Min(rows * ItemOffset.Y - 480 + 24 + 60, y);
-			camera.Src = new RectI(
-				camera.Src.X,
-				(int)y,
-				camera.Src.Width,
-				camera.Src.Height);
+            y = Math.Max(0, y);
+            y = Math.Min(rows * ItemOffset.Y - 480 + 24 + 60, y);
+            camera.Src = new RectI(
+                camera.Src.X,
+                (int)y,
+                camera.Src.Width,
+                camera.Src.Height);
 
-			var mouse = new CircleShape()
-			{
-				Position = Engine.Mouse.Position + new Vector2DF(0, camera.Src.Y),
-				OuterDiameter = 2,
-			};
+            var mouse = new CircleShape()
+            {
+                Position = Engine.Mouse.Position + new Vector2DF(0, camera.Src.Y),
+                OuterDiameter = 2,
+            };
 
-			if(activeItem != null && !activeItem.Shape.GetIsCollidedWith(mouse))
-			{
-				activeItem.Disactivate();
-				activeItem = null;
+            if (activeItem != null && !activeItem.Shape.GetIsCollidedWith(mouse))
+            {
+                activeItem.Disactivate();
+                activeItem = null;
                 SelectionChanged(null);
             }
 
-			foreach(var item in items)
-			{
-				if(item.Shape.GetIsCollidedWith(mouse))
-				{
-                    if(item != activeItem)
+            foreach (var item in items)
+            {
+                if (item.Shape.GetIsCollidedWith(mouse))
+                {
+                    if (item != activeItem)
                     {
                         item.Activate();
                         activeItem = item;
                         SelectionChanged(item.Sample);
                     }
-					if(OnDecide != null && Engine.Mouse.LeftButton.ButtonState == MouseButtonState.Push)
-					{
-						OnDecide(item.Sample);
-					}
-					break;
-				}
-			}
-		}
-	}
+                    if (OnDecide != null && Engine.Mouse.LeftButton.ButtonState == MouseButtonState.Push)
+                    {
+                        OnDecide(item.Sample);
+                    }
+                    break;
+                }
+            }
+        }
+    }
 }
--- Sample/ApplicationSample/sample_cs/Browser/SampleItem.cs
+++ Sample/ApplicationSample/sample_cs/Browser/SampleItem.cs
@@ -7,102 +7,119 @@ using asd;
 
 namespace sample_cs
 {
-	class SampleItem : TextureObject2D
-	{
-		public static readonly Vector2DF Size = new Vector2DF(150, 112.5f);
-		public static int TitleMaxLength = 9;
+    class SampleItem : TextureObject2D
+    {
+        public static readonly Vector2DF Size = new Vector2DF(150, 112.5f);
+        public static int TitleMaxLength = 9;
 
-		private RectangleShape shape_;
-		private TextureObject2D frame { get; set; }
+        private RectangleShape shape_;
+        private TextureObject2D frame { get; set; }
 
-		public ISample Sample { get; private set; }
-		public RectangleShape Shape
-		{
-			get { return shape_ = shape_ ?? GetArea(); }
-		}
-		public TextObject2D Title { get; private set; }
+        public ISample Sample { get; private set; }
+        public RectangleShape Shape
+        {
+            get
+            {
+                if (shape_ == null)
+                {
+                    shape_ = GetArea();
+                }
+                return shape_;
+            }
 
-		public new Vector2DF Position
-		{
-			get { return base.Position; }
-			set
-			{
-				base.Position = value;
-				shape_ = null;
-			}
-		}
+        }
+        public TextObject2D Title { get; private set; }
 
-		public SampleItem(ISample sample, Font font)
-		{
-			Sample = sample;
+        public new Vector2DF Position
+        {
+            get { return base.Position; }
+            set
+            {
+                base.Position = value;
+                shape_ = null;
+            }
+        }
 
-			Texture = Engine.Graphics.CreateTexture2D("Data/Browser/" + sample.GetType().Name + ".png");
-			if(Texture == null)
-			{
-				Texture = Engine.Graphics.CreateTexture2D("Data/Browser/Default.png");
-			}
-			Scale = Size / Texture.Size.To2DF();
+        public SampleItem(ISample sample, Font font)
+        {
+            Sample = sample;
 
-			var text = sample.Title != "" ? sample.Title : sample.GetType().Name;
-			Title = new TextObject2D()
-			{
-				Text = GetWrappedString(font, text),
-				Font = font,
-				Position = new Vector2DF(0, 115),
-				DrawingPriority = 1,
-			};
-			AddChild(Title, ChildManagementMode.Nothing, ChildTransformingMode.Position);
+            Texture = Engine.Graphics.CreateTexture2D("Data/Browser/" + sample.GetType().Name + ".png");
+            if (Texture == null)
+            {
+                Texture = Engine.Graphics.CreateTexture2D("Data/Browser/Default.png");
+            }
+            Scale = Size / Texture.Size.To2DF();
 
-			frame = new TextureObject2D()
-			{
-				Texture = Engine.Graphics.CreateTexture2D("Data/Browser/Frame.png"),
-				Position = new Vector2DF(-3, -3),
-			};
-			AddChild(frame, ChildManagementMode.Nothing, ChildTransformingMode.Position);
-		}
+            string text;
+            if (sample.Title != "")
+            {
+                text = sample.Title;
+            }
+            else
+            {
+                text = sample.GetType().Name;
+            }
 
-		public RectangleShape GetArea()
-		{
-			return new RectangleShape
-			{
-				DrawingArea = new RectF(Position.X, Position.Y, Size.X, Size.Y),
-			};
-		}
+            Title = new TextObject2D()
+            {
+                Text = GetWrappedString(font, text),
+                Font = font,
+                Position = new Vector2DF(0, 115),
+                DrawingPriority = 1,
+            };
+            AddChild(Title, ChildManagementMode.Nothing, ChildTransformingMode.Position);
 
-		private string GetWrappedString(Font font, string title)
-		{
-			string result = title;
-			if(font.CalcTextureSize(result, WritingDirection.Horizontal).X <= Size.X)
-			{
-				return result;
-			}
-			while(font.CalcTextureSize(result + "…", WritingDirection.Horizontal).X > Size.X)
-			{
-				result = result.Substring(0, result.Length - 1);
-			}
-			return result + "…";
-		}
+            frame = new TextureObject2D()
+            {
+                Texture = Engine.Graphics.CreateTexture2D("Data/Browser/Frame.png"),
+                Position = new Vector2DF(-3, -3),
+            };
+            AddChild(frame, ChildManagementMode.Nothing, ChildTransformingMode.Position);
+        }
 
-		protected override void OnAdded()
-		{
-			Layer.AddObject(Title);
-			Layer.AddObject(frame);
-		}
+        public RectangleShape GetArea()
+        {
+            return new RectangleShape
+            {
+                DrawingArea = new RectF(Position.X, Position.Y, Size.X, Size.Y),
+            };
+        }
 
-		protected override void OnDispose()
-		{
-			Title.Dispose();
-			frame.Dispose();
-		}
+        private string GetWrappedString(Font font, string title)
+        {
+            string result = title;
+            if (font.CalcTextureSize(result, WritingDirection.Horizontal).X <= Size.X)
+            {
+                return result;
+            }
+            while (font.CalcTextureSize(result + "…", WritingDirection.Horizontal).X > Size.X)
+            {
+                result = result.Substring(0, result.Length - 1);
+            }
+            return result + "…";
+        }
 
-		public void Activate()
-		{
-			frame.Texture = Engine.Graphics.CreateTexture2D("Data/Browser/FrameActive.png");
-		}
+        protected override void OnAdded()
+        {
+            Layer.AddObject(Title);
+            Layer.AddObject(frame);
+        }
 
-		public void Disactivate()
-		{
-			frame.Texture = Engine.Graphics.CreateTexture2D("Data/Browser/Frame.png");
-		}
-	}
+        protected override void OnDispose()
+        {
+            Title.Dispose();
+            frame.Dispose();
+        }
+
+        public void Activate()
+        {
+            frame.Texture = Engine.Graphics.CreateTexture2D("Data/Browser/FrameActive.png");
+        }
+
+        public void Disactivate()
+        {
+            frame.Texture = Engine.Graphics.CreateTexture2D("Data/Browser/Frame.png");
+        }
+    }
 }
