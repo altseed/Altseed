791f37a70195ab37574f8b9c193d736459fa20fb
ArGxento <argxento@gmail.com>
2017-03-07 21:51:37 +0900

    ToU16StringでのMSVCのバグに対するワークアラウンドを実装

---
+++ Dev/unitTest_cpp_gtest/Utility/asd.ToU16StringTest.cpp
@@ -0,0 +1,12 @@
+﻿#include<gtest/gtest.h>
+#include<Utility/asd.ToU16String.h>
+#include<string>
+
+TEST(Utility, ToU16StringTest) {
+	using namespace std::string_literals;
+	ASSERT_EQ(asd::ToU16String(42), u"42"s);
+	ASSERT_EQ(asd::ToU16String(31337L), u"31337"s);
+	ASSERT_EQ(asd::ToU16String(3.14).substr(0, 4), u"3.14"s); // compare only first 4 chars because we don't know the number of zeros trailing
+	ASSERT_EQ(asd::ToU16String(3.14f).substr(0, 4), u"3.14"s);
+
+}
