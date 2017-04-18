1a96306f9667645bf2abda5067bcd1cac95eb060
durswd <swda.durl@gmail.com>
2017-04-18 22:20:41 +0900

    Change enum

--- Dev/asd_cpp/core/Input/asd.Joystick.h
+++ Dev/asd_cpp/core/Input/asd.Joystick.h
@@ -8,24 +8,24 @@ namespace asd{
 	enum class JoystickButtonState : int32_t
 	{
 		/**
-		@brief	ボタンをこのフレームで押した状態
+		@brief ボタンをこのフレームで押した状態
 		*/
 		Push,
 
 		/**
-		@brief	ボタンをこのフレームで離した状態
+		@brief ボタンをこのフレームで離した状態
 		*/
 		Release,
 
 		/**
-		@brief	ボタンを離し続けている状態
+		@brief ボタンを押し続けている状態
 		*/
-		Free,
+		Hold,
 
 		/**
-		@brief	ボタンを押し続けている状態
+		@brief ボタンを離し続けている状態
 		*/
-		Hold
+		Free
 	};
 
 	/**
