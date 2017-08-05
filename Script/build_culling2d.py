442798ea3ea79d52c4156a3b1e1be2c5d7caf7ff
Asato Imanishi <dtm.music.producer@gmail.com>
2017-08-05 18:10:09 +0900

    Culling2Dを新たなビルド構成に適応

--- Script/build_culling2d.py
+++ Script/build_culling2d.py
@@ -50,12 +50,20 @@ with aceutils.CurrentDir('../Downloads'):
 		aceutils.mkdir(r'../Dev/lib/x64/Release')
 
 		aceutils.copy(r'Culling2D/Culling2D/Culling2D.h', r'../Dev/include/')
+		aceutils.copy(r'Culling2D/culling2d_common/src/Culling2DCommon.h', r'../Dev/include/')
 
 		aceutils.copy(r'culling2d_bin/Debug/Culling2D_debug.lib', r'../Dev/lib/x86/Debug/Culling2D.lib')
 		aceutils.copy(r'culling2d_bin/Release/Culling2D.lib', r'../Dev/lib/x86/Release/')
 		aceutils.copy(r'culling2d_bin_x64/Debug/Culling2D_debug.lib', r'../Dev/lib/x64/Debug/Culling2D.lib')
 		aceutils.copy(r'culling2d_bin_x64/Release/Culling2D.lib', r'../Dev/lib/x64/Release/')
 
+		aceutils.copy(r'culling2d_bin/Debug/Culling2D_AABB_debug.lib', r'../Dev/lib/x86/Debug/')
+		aceutils.copy(r'culling2d_bin/Release/Culling2D_AABB.lib', r'../Dev/lib/x86/Release/')
+		aceutils.copy(r'culling2d_bin_x64/Debug/Culling2D_AABB_debug.lib', r'../Dev/lib/x64/Debug/')
+		aceutils.copy(r'culling2d_bin_x64/Release/Culling2D_AABB.lib', r'../Dev/lib/x64/Release/')
+
 	else:
 		aceutils.copy(r'Culling2D/Culling2D/Culling2D.h', r'../Dev/include/')
+		aceutils.copy(r'Culling2D/culling2d_common/src/Culling2DCommon.h', r'../Dev/include/')
+		aceutils.copy(r'culling2d_bin/bin/libCulling2D_AABB', r'../Dev/lib/')
 		aceutils.copy(r'culling2d_bin/bin/libCulling2D.a', r'../Dev/lib/')
