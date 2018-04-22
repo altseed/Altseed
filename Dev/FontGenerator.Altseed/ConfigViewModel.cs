0ae4abf5627ab48fc8afaf4a401787b2a7c2f94f
NumAniCloud <num.ani.cloud@gmail.com>
2018-04-22 19:40:21 +0900

    imguiを使ったフォントジェネレータを作成中

---
+++ Dev/FontGenerator.Altseed/ConfigViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FontGenerator.Altseed
+{
+	class ConfigViewModel
+	{
+		public static readonly int TextSize = 2048;
+
+		public int FontIndex = 0;
+		public sbyte[] TextPath = new sbyte[TextSize];
+		public sbyte[] ExportPath = new sbyte[TextSize];
+		public sbyte[] SheetName = new sbyte[TextSize];
+		public int FontSize = 1;
+		public int TextureSize = 256;
+		public int OutlineSize = 0;
+		public int OutlineSampling = 1;
+		public asd.Color FontColor = new asd.Color(255, 255, 255, 255);
+		public asd.Color OutlineColor = new asd.Color(0, 0, 0, 255);
+	}
+}
---
+++ Dev/FontGenerator.Altseed/Extensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FontGenerator.Altseed
+{
+	static class Extensions
+	{
+		public static bool InputInt(this asd.Tool tool, string label, ref int value)
+		{
+			var buf = new int[1] { value };
+			var changed = tool.InputInt(label, buf);
+			value = buf[0];
+			return changed;
+		}
+
+		public static bool Color(this asd.Tool tool, string label, ref asd.Color color)
+		{
+			var buf = new float[4] 
+			{
+				color.R / 255.0f,
+				color.G / 255.0f,
+				color.B / 255.0f,
+				color.A / 255.0f
+			};
+			var changed = tool.ColorEdit4(label, buf);
+			color = new asd.Color((byte)(buf[0] * 255),
+				(byte)(buf[1] * 255),
+				(byte)(buf[2] * 255),
+				(byte)(buf[3] * 255));
+			return changed;
+		}
+	}
+}
