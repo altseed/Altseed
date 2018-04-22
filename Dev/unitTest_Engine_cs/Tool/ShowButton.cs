c5a98efa97d1aa26f5f38570734451ef68557a74
NumAniCloud <num.ani.cloud@gmail.com>
2018-04-22 18:55:16 +0900

    テストを充実

--- Dev/unitTest_Engine_cs/Tool/ShowButton.cs
+++ Dev/unitTest_Engine_cs/Tool/ShowButton.cs
@@ -9,9 +9,23 @@ namespace unitTest_Engine_cs.Tool
 	class ShowButton : EngineTest
 	{
 		private asd.TextureObject2D obj;
+		private sbyte[] inputText = new sbyte[128];
+		private int[] inputInt = new int[] { 99 };
+		private float[] color = new float[4] { 0, 0, 0, 0 };
+		private int[] selection = new int[1];
+		private string list;
+		private asd.Texture2D texture;
 
-		public ShowButton() : base(120)
+		public ShowButton() : base(600)
 		{
+			var listArray = new string[4]
+			{
+				"Plain Propton",
+				"Raster Road",
+				"Octern Ocean",
+				"Gigant Ground",
+			};
+			list = string.Join(";", listArray);
 		}
 
 		protected override void OnStart()
@@ -25,15 +39,41 @@ namespace unitTest_Engine_cs.Tool
 			asd.Engine.AddObject2D(obj);
 
 			asd.Engine.OpenTool();
+
+			texture = asd.Engine.Graphics.CreateTexture2D("Data/Texture/Sample1.png");
 		}
 
 		protected override void OnUpdating()
 		{
 			asd.Engine.Tool.Begin("ShowButton");
 
-			if (asd.Engine.Tool.Button("Push me!"))
 			{
-				obj.IsDrawn = true;
+				asd.Engine.Tool.Text("It's text!");
+
+				if (asd.Engine.Tool.Button("Push me!"))
+				{
+					obj.IsDrawn = true;
+				}
+
+				/*
+				asd.Engine.Tool.BeginCombo("Combo!", "Select");
+
+				{
+					asd.Engine.Tool.Text("Plain Propton");
+					asd.Engine.Tool.Text("Raster Road");
+					asd.Engine.Tool.Text("Octern Ocean");
+					asd.Engine.Tool.Text("Gigant Ground");
+				}
+
+				asd.Engine.Tool.EndCombo();
+				//*/
+
+				asd.Engine.Tool.Image(texture, new asd.Vector2DF(100, 100));
+				
+				asd.Engine.Tool.InputText("Enter text!", inputText, inputText.Length);
+				asd.Engine.Tool.InputInt("Enter integer!", inputInt);
+				asd.Engine.Tool.ColorEdit4("Color", color);
+				asd.Engine.Tool.ListBox("List", selection, list);
 			}
 
 			asd.Engine.Tool.End();
