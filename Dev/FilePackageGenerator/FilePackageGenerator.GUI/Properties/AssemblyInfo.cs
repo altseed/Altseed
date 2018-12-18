ebbf9265f645377e66afa5111d7e3b82c558bb66
wraikny <writtenknown@gmail.com>
2018-12-19 03:32:34 +0900

    update copyright year and format

--- Dev/FilePackageGenerator/FilePackageGenerator.GUI/Properties/AssemblyInfo.cs
+++ Dev/FilePackageGenerator/FilePackageGenerator.GUI/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("FilePackagingTool.GUI")]
-[assembly: AssemblyCopyright("Copyright © 2013 - 2016 Altseed")]
+[assembly: AssemblyCopyright("Copyright © 2013 - 2018 Altseed")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
