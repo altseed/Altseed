e11a39e25865fc095ff5673566ce075a605adb25
durswd <swda.durl@gmail.com>
2019-04-09 01:07:43 +0900

    fixed script

--- Script/build_bullet.py
+++ Script/build_bullet.py
@@ -12,21 +12,31 @@ with aceutils.CurrentDir('../Downloads'):
 	aceutils.mkdir(r"bullet_bin")
 	aceutils.mkdir(r"bullet_bin_x64")
 
+	options = []
+	options += ['-D', 'BUILD_PYBULLET=OFF']
+	options += ['-D', 'BUILD_EXTRAS=OFF']
+	options += ['-D', 'BUILD_UNIT_TESTS=OFF']
+	options += ['-D', 'BUILD_CPU_DEMOS=OFF']
+	options += ['-D', 'BUILD_BULLET2_DEMOS=OFF']
+	options += ['-D', 'BUILD_OPENGL3_DEMOS=OFF']
+	options += ['-D', 'USE_GRAPHICAL_BENCHMARK=OFF']
+	options_cmd = ' '.join(options)
+
 	with aceutils.CurrentDir('bullet_bin'):
 		if aceutils.isWin():
-			aceutils.call(aceutils.cmd_cmake+r'-D USE_MSVC_RUNTIME_LIBRARY_DLL:BOOL=OFF -D BUILD_DEMOS:BOOL=OFF ../bullet3/')
+			aceutils.call(aceutils.cmd_cmake+r'-D USE_MSVC_RUNTIME_LIBRARY_DLL:BOOL=OFF ' + options_cmd + r' ../bullet3/')
 			aceutils.call('cmake --build . --config Debug')
 			aceutils.call('cmake --build . --config Release')
 		elif aceutils.isMac():
-			aceutils.call(r'cmake -G "Unix Makefiles" -D CMAKE_BUILD_TYPE=Release -D USE_MSVC_RUNTIME_LIBRARY_DLL:BOOL=OFF -D USE_INTERNAL_LOADER:BOOL=OFF -DBUILD_SHARED_LIBS=OFF -DBUILD_PYBULLET=OFF "-DCMAKE_OSX_ARCHITECTURES=x86_64' + (';i386' if aceutils.Isi386() else '') + r'" ../bullet3/')
+			aceutils.call(r'cmake -G "Unix Makefiles" -D CMAKE_BUILD_TYPE=Release ' + options_cmd + r'  "-DCMAKE_OSX_ARCHITECTURES=x86_64' + (';i386' if aceutils.Isi386() else '') + r'" ../bullet3/')
 			aceutils.call(r'make')
 		else:
-			aceutils.call(r'cmake -G "Unix Makefiles" -D CMAKE_BUILD_TYPE=Release -D USE_MSVC_RUNTIME_LIBRARY_DLL:BOOL=OFF ../bullet3/')
+			aceutils.call(r'cmake -G "Unix Makefiles" -D CMAKE_BUILD_TYPE=Release ' + options_cmd + r' ../bullet3/')
 			aceutils.call(r'make')
 
 	with aceutils.CurrentDir('bullet_bin_x64'):
 		if aceutils.isWin():
-			aceutils.call(aceutils.cmd_cmake_x64+r'-D USE_MSVC_RUNTIME_LIBRARY_DLL:BOOL=OFF -D BUILD_DEMOS:BOOL=OFF ../bullet3/')
+			aceutils.call(aceutils.cmd_cmake_x64+r'-D USE_MSVC_RUNTIME_LIBRARY_DLL:BOOL=OFF ' + options_cmd + r' ../bullet3/')
 			aceutils.call('cmake --build . --config Debug')
 			aceutils.call('cmake --build . --config Release')
 
